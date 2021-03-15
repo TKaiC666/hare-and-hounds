c02bd7bc32585c1a3432ca1229e257d965ea1fa9
KaiC <tkaic666@gmail.com>
2021-03-16 00:29:43 +0800

    JavaScript : * 更名 units -> pieces            rabbit -> hare * peice物件增加對應的html element * 錯字修正

--- js/main.js
+++ js/main.js
@@ -8,62 +8,64 @@ function CreateGamesystem(){
     let GS = {};
     GS.field;
     GS.hounds = new Array(3);
-    GS.rabbit;
+    GS.hare;
     GS.player;
     GS.CPU;
 
     GS.initial = function(){
 
-        console.log("intial start");
+        console.warn("initial start");
 
         GS.field = new Field();
         for(var i = 0; i < GS.hounds.length; i++){
             GS.hounds[i] = new Pawn("hound",i+1);
         }
-        GS.rabbit = new Pawn("rabbit");
-        GS.player = GS.rabbit.type;
+        GS.hare = new Pawn("hare");
+        GS.player = GS.hare.type;
         GS.CPU = GS.hounds[0].type;
         
         function Field(){
-            this.units = new Array(10);
-            this.units[0] = new Unit([1,2,3],true);
-            this.units[1] = new Unit([0,2,4,5],true);
-            this.units[2] = new Unit([0,1,3,5],true);
-            this.units[3] = new Unit([0,2,5,6],true);
-            this.units[4] = new Unit([1,5,7],true);
-            this.units[5] = new Unit([1,2,3,4,6,7,8,9],true);
-            this.units[6] = new Unit([3,5,9],true);
-            this.units[7] = new Unit([4,5,8,10],true);
-            this.units[8] = new Unit([5,7,9,10],true);
-            this.units[9] = new Unit([5,6,8,10],true);
-            this.units[10] = new Unit([7,8,9],true);
+            this.pieces = new Array(10);
+            this.pieces[0] = new Piece(0,[1,2,3],true);
+            this.pieces[1] = new Piece(1,[0,2,4,5],true);
+            this.pieces[2] = new Piece(2,[0,1,3,5],true);
+            this.pieces[3] = new Piece(3,[0,2,5,6],true);
+            this.pieces[4] = new Piece(4,[1,5,7],true);
+            this.pieces[5] = new Piece(5,[1,2,3,4,6,7,8,9],true);
+            this.pieces[6] = new Piece(6,[3,5,9],true);
+            this.pieces[7] = new Piece(7,[4,5,8,10],true);
+            this.pieces[8] = new Piece(8,[5,7,9,10],true);
+            this.pieces[9] = new Piece(9,[5,6,8,10],true);
+            this.pieces[10] = new Piece(10,[7,8,9],true);
             this.getMovable = function(index){
                 let arr = new Array();
-                let target = this.units[index].connectTo;
+                let target = this.pieces[index].connectTo;
                 for(var i = 0; i < target.length; i++){
-                    if(this.units[target[i]].isEmpty) arr.push(target[i]);
+                    if(this.pieces[target[i]].isEmpty) arr.push(target[i]);
                 }
                 return arr;
             }
-            this.getUnits = function(){
-                return this.units;
+            this.getPieces = function(){
+                return this.pieces;
             }
         
-            function Unit(connectList,isEmpty){
+            function Piece(index,connectList,isEmpty){
                 this.connectTo = connectList;
                 this.isEmpty = isEmpty;
-                console.log("Create new unit object");
+                this.element = document.getElementsByClassName("piece")[index];
+
+                console.log("Create new piece");
             }
             
         
-            console.log("Create new field array");
+            console.log("Create new field");
         }
 
         function Pawn(type,no){
             let index = 0;
             this.type=type;
         
-            if(type === "rabbit"){
+            if(type === "hare"){
                 index = 0;
             }
             else if(type === "hound"){
@@ -82,7 +84,7 @@ function CreateGamesystem(){
             }
         
             this.position = index;
-            this.positionData = GS.field.units[this.position];
+            this.positionData = GS.field.pieces[this.position];
             this.positionData.isEmpty = false;
             this.positionData.pawn = type;
         
@@ -92,7 +94,7 @@ function CreateGamesystem(){
                 delete this.positionData.pawn;
         
                 this.position = index;
-                this.positionData = GS.field.units[this.position];
+                this.positionData = GS.field.pieces[this.position];
                 this.positionData.isEmpty = false;
                 this.positionData.pawn = type;
                 console.log(`${this.type}${this.number !== undefined ? this.number : ""} move from ${old} to ${this.position}`);
@@ -101,7 +103,7 @@ function CreateGamesystem(){
             console.log(`Create new ${this.type} pawn ${this.number !== undefined ? "No."+this.number : ""}`);
         }
         
-        console.log("intial finish");
+        console.warn("initial finish");
 
     };
     
@@ -110,47 +112,48 @@ function CreateGamesystem(){
     //遊戲規則是獵犬先手
     GS.running = function(){
 
-        console.log("running game...");
+        console.warn("running game...");
 
         let player;
         let CPU;
 
-        pawnSetting("rabbit");
-        while(judgement){
-            houndsTurn();
-            rabbitTurn();
-            console.log(this.field.getUnits());
-        }
+        pawnSetting("hare");
+        // while(judgement){
+        //     houndsTurn();
+        //     hareTurn();
+        //     console.log(this.field.getPieces());
+        // }
         // houndsTurn();
-        // rabbitTurn();
+        // hareTurn();
         return true;
 
         function pawnSetting(playerSelec){
             player = playerSelec;
-            CPU = player === "rabbit" ? "hounds" : "rabbit";
+            CPU = player === "hare" ? "hounds" : "hare";
             console.log(`${player} is player, ${CPU} is CPU`);
         }
 
         function houndsTurn(){
             let randNum = Math.floor(Math.random() * 3);
-            let moveRange = GS.field.units[GS.hounds[randNum].position].connectTo;
+            let moveRange = GS.field.pieces[GS.hounds[randNum].position].connectTo;
             let randTarget = Math.floor(Math.random() * moveRange.length);
             let newPosition;
-            while(GS.field.units[moveRange[randTarget]].isEmpty !== true){
+            while(GS.field.pieces[moveRange[randTarget]].isEmpty !== true){
                 randTarget = Math.floor(Math.random() * moveRange.length);
             }
             newPosition = moveRange[randTarget];
             GS.hounds[randNum].moveTo(newPosition);
+
         }
 
-        function rabbitTurn(){
+        function hareTurn(){
             //for test
-            let targetIndex = GS.rabbit.position;
+            let targetIndex = GS.hare.position;
             let possibleRange = GS.field.getMovable(targetIndex);
             console.log(possibleRange);
             let hint = possibleRange;
             let newPosition = prompt(`請問下一步要往?\n可前往${hint}\n`);
-            GS.rabbit.moveTo(newPosition);
+            GS.hare.moveTo(newPosition);
         }
 
         function judgement(){
