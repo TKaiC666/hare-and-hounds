0f70ab9df4dde6f7b45cbbb84a2e7496600ad798
KaiC <tkaic666@gmail.com>
2021-03-19 23:25:20 +0800

    JavaScript : 兔子和獵犬會顯示在畫面上了 ! Pawn新增interface function；init()和draw() Hare和Hound會實作此function。

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 //=====================objects=====================
-function Piece(surrounding, isEmpty, x, y, size){
+function Piece(surrounding, isEmpty, pawn, x, y, size){
     this.surrounding = surrounding;
     this.isEmpty = isEmpty;
+    this.pawn = pawn;
     this.x = x;
     this.y = y;
     this.size = size;
@@ -20,40 +21,52 @@ Piece.prototype.draw = function(){
     ctx.fillStyle = 'white';
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-}
+    if(this.pawn !== null){
+        //在init前會執行一次。但是這時image沒有src，所以圖跑不出來。
+        this.pawn.draw();
+    }
+};
 
 function Field(){
     this.pieces = new Array(11);
-    this.pieces[0] = new Piece([1,2,3], true, 228, 377, 70);
-    this.pieces[1] = new Piece([0,2,4,5], true, 498, 149, 70);
-    this.pieces[2] = new Piece([0,1,3,5], true, 498, 377, 70);
-    this.pieces[3] = new Piece([0,2,5,6], true, 498, 605, 70);
-    this.pieces[4] = new Piece([1,5,7], true, 768, 149, 70);
-    this.pieces[5] = new Piece([1,2,3,4,6,7,8,9], true, 768, 377, 70);
-    this.pieces[6] = new Piece([3,5,9], true, 768, 605, 70);
-    this.pieces[7] = new Piece([4,5,8,10], true, 1038, 149, 70);
-    this.pieces[8] = new Piece([5,7,9,10], true, 1038, 377, 70);
-    this.pieces[9] = new Piece([5,6,8,10], true, 1038, 605, 70);
-    this.pieces[10] = new Piece([7,8,9], true, 1308, 377, 70);
-    this.hare = new Hare(158, 307);
+    this.pieces[0] = new Piece([1,2,3], false, new Hare(158, 307), 228, 377, 70);
+    this.pieces[1] = new Piece([0,2,4,5], true, null, 498, 149, 70);
+    this.pieces[2] = new Piece([0,1,3,5], true, null, 498, 377, 70);
+    this.pieces[3] = new Piece([0,2,5,6], true, null, 498, 605, 70);
+    this.pieces[4] = new Piece([1,5,7], true, null, 768, 149, 70);
+    this.pieces[5] = new Piece([1,2,3,4,6,7,8,9], true, null, 768, 377, 70);
+    this.pieces[6] = new Piece([3,5,9], true, null, 768, 605, 70);
+    this.pieces[7] = new Piece([4,5,8,10], false, new Hound(968, 79, 0), 1038, 149, 70);
+    this.pieces[8] = new Piece([5,7,9,10], true, null, 1038, 377, 70);
+    this.pieces[9] = new Piece([5,6,8,10], false, new Hound(968, 535, 1), 1038, 605, 70);
+    this.pieces[10] = new Piece([7,8,9], false, new Hound(1238, 307, 2), 1308, 377, 70);
 }
 
 Field.prototype.init = function(){
     this.draw();
-    this.hare.init();
-}
+    let pieces = this.pieces;
+    for( var i = 0; i < pieces.length; i++){
+        if(pieces[i].pawn !== null){
+            pieces[i].pawn.init();
+        } 
+    }
+};
 
 Field.prototype.draw = function(){
     let pieces = this.pieces;
     for(var i = 0; i < pieces.length; i++){
         pieces[i].draw();
     }
-}
+};
 
 function Pawn(x, y){
     this.x = x;
     this.y = y;
 }
+
+Pawn.prototype.init = function(){};
+
+Pawn.prototype.draw = function(){};
 
 function Hare(x, y){
     Pawn.call(this,x ,y);
@@ -70,18 +83,33 @@ Hare.prototype.init = function(){
         ctx.drawImage(this, x, y);
     });
     this.img.src = this.imgSrc;
-}
+};
 
 Hare.prototype.draw = function(){
     ctx.drawImage(this.img, this.x, this.y);
-}
+};
 
 function Hound(x, y, index){
     Pawn.call(this, x, y);
     this.index = index;
+    this.img = new Image();
+    this.imgSrc = 'images/hound.png';
 }
 Hound.prototype = Object.create(Pawn.prototype);
 Hound.prototype.constructor = Hound;
+
+Hound.prototype.init = function(){
+    let x = this.x;
+    let y = this.y;
+    this.img.addEventListener('load', function(){
+        ctx.drawImage(this, x, y);
+    });
+    this.img.src = this.imgSrc;
+};
+
+Hound.prototype.draw = function(){
+    ctx.drawImage(this.img, this.x, this.y);
+};
 
 //=====================running=====================
 
@@ -90,3 +118,11 @@ ctx.fillRect(0, 0, width, height);
 
 let field = new Field();
 field.init();
+
+function loop(){
+    field.draw();
+
+    requestAnimationFrame(loop);
+}
+
+//loop();
